0f09f00eeb5e1238d1588cf2e26e486f84f96d51
Kate <9zink9@gmail.com>
2015-09-10 19:51:14 +0300

    *

---
+++ _protected/config/db.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=play',
+    'username' => 'root',
+    'password' => 'sql111',
+    'charset' => 'utf8',
+];
